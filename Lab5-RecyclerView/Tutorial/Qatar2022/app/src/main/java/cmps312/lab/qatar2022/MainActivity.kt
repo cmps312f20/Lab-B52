d71a7f790234cb852573560b2d468d7b531536f7
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-16 15:53:07 +0300

    update

--- Lab5-RecyclerView/Tutorial/Qatar2022/app/src/main/java/cmps312/lab/qatar2022/MainActivity.kt
+++ Lab5-RecyclerView/Tutorial/Qatar2022/app/src/main/java/cmps312/lab/qatar2022/MainActivity.kt
@@ -1,12 +1,51 @@
 package cmps312.lab.qatar2022
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import cmps312.lab.qatar2022.model.StadiumRepository
 import com.example.qatar2022.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.list_item_stadium.*
 
 class MainActivity : AppCompatActivity() {
+    var currentIndex = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        StadiumRepository.initStadiums(this)
+        displayStadium()
+
+        nextBtn.setOnClickListener {
+            currentIndex++
+            displayStadium()
+        }
+
+        prevBtn.setOnClickListener {
+            currentIndex--
+            displayStadium()
+        }
     }
+
+
+    fun displayStadium() {
+        StadiumRepository.apply {
+
+            currentIndex = (currentIndex + stadiums.size) % stadiums.size //java+c++
+
+            nameTv.text = stadiums[currentIndex].name
+            cityTv.text = stadiums[currentIndex].city
+            statusTv.text = stadiums[currentIndex].status
+
+            val imageName = stadiums[currentIndex].imageName
+            val imageId = resources.getIdentifier(imageName, "drawable", packageName)
+            stadiumIv.setImageResource(imageId)
+
+            currentIndexTv.text = "${currentIndex + 1} of ${stadiums.size}"
+        }
+
+    }
+
+
 }
--- Lab5-RecyclerView/Tutorial/Qatar2022/app/src/main/java/cmps312/lab/qatar2022/model/StadiumRepository.kt
+++ Lab5-RecyclerView/Tutorial/Qatar2022/app/src/main/java/cmps312/lab/qatar2022/model/StadiumRepository.kt
@@ -1,16 +1,24 @@
 package cmps312.lab.qatar2022.model
 
 import android.content.Context
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 
 //reading the data from the json and converting to list
 object StadiumRepository {
-    val stadiums = listOf<Stadium>();
+    var stadiums = listOf<Stadium>();
 
     fun initStadiums(context: Context){
         val data = context.assets
             .open("stadiums.json")
             .bufferedReader()
             .use { it.readText() }
+
+        //change it to stadium list object
+        stadiums = Json {ignoreUnknownKeys = true}
+            .decodeFromString(data)//4 -> 2
+
     }
+
 }
