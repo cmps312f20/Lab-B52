0038d716aa26d143cf3dbc16c2a6c3388664bad3
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-23 22:08:02 +0300

    Update MainActivity.kt

--- Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/MainActivity.kt
+++ Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package cmps312.lab.bankingapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -11,7 +13,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , NavController.OnDestinationChangedListener {
 
     lateinit var drawerLayout : DrawerLayout
     lateinit var appBarConfiguration : AppBarConfiguration
@@ -29,10 +31,21 @@ class MainActivity : AppCompatActivity() {
         navigationView.setupWithNavController(navController)
 
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
         setupActionBarWithNavController(navController , appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+
+        Toast.makeText(this, destination.label, Toast.LENGTH_SHORT).show()
+
     }
 }
