67d87c932e0f188fcc01c64501c454bd89b8a73b
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-23 16:11:36 +0300

    update 4:!1

--- Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/TransferInfoFragment.kt
+++ Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/TransferInfoFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_transfer_info.view.*
 
 class TransferInfoFragment : Fragment(R.layout.fragment_transfer_info) {
 
@@ -16,7 +18,17 @@ class TransferInfoFragment : Fragment(R.layout.fragment_transfer_info) {
         super.onViewCreated(view, savedInstanceState)
 
         view.apply {
-            Log.d("TAG", "onViewCreated: " + args.transaction.senderName)
+            val transaction = args.transaction
+            nextBtn.setOnClickListener {
+               transaction.receiverName = recieverNameEdt.text.toString()
+               transaction.receiverAccountNo = recieverAccountNoEdt.text.toString()
+
+                val action = TransferInfoFragmentDirections
+                    .actionTransferInfoFragmentToTransferConfirmationFragment(transaction)
+
+                findNavController().navigate(action)
+            }
+
         }
     }
 }
