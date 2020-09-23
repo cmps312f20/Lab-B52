906835020df9fe19d616cd28c9209c8f37ff5fac
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-23 16:39:03 +0300

    update 4:39

--- Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/TransferConfirmationFragment.kt
+++ Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/TransferConfirmationFragment.kt
@@ -5,15 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_transfer_confirmation.*
 
 
-class TransferConfirmationFragment : Fragment() {
+class TransferConfirmationFragment : Fragment(R.layout.fragment_transfer_confirmation) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transfer_confirmation, container, false)
+    val args: TransferConfirmationFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.apply {
+            senderNameTv.text = args.transaction.senderName
+        }
     }
 }
