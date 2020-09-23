d9e1e99db79d7f5238603c02f2075732d47b43a8
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-23 16:53:04 +0300

    update

--- Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/MainActivity.kt
+++ Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/MainActivity.kt
@@ -1,15 +1,38 @@
 package cmps312.lab.bankingapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import cmps312.lab.bankingapp.model.Transaction
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
+    lateinit var drawerLayout : DrawerLayout
+    lateinit var appBarConfiguration : AppBarConfiguration
+    lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerlayout)
+        navController = findNavController(R.id.fragment)
+
+        //menu ->
+        navigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController , appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp()
+
     }
 }
