af321447188e3c478e33e04998f79326f845d2f0
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-30 16:28:22 +0300

    update

--- Lab7-MVVM/Tutorial/BankingApp/app/src/main/java/cmps312/lab/bankingapp/ui/transfer/viewmodel/TransferViewModel.kt
+++ Lab7-MVVM/Tutorial/BankingApp/app/src/main/java/cmps312/lab/bankingapp/ui/transfer/viewmodel/TransferViewModel.kt
@@ -4,17 +4,22 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import cmps312.lab.bankingapp.model.Account
 import cmps312.lab.bankingapp.model.Transfer
 import cmps312.lab.bankingapp.reposiotry.BankRepository
 
 class TransferViewModel(context: Application) : AndroidViewModel(context) {
     private var _transfers = MutableLiveData<MutableList<Transfer>>() //null
     lateinit var selectedTransfer : Transfer //null
+    lateinit var accounts : List<Account>
+    lateinit var transfer: Transfer
 
     init {
         BankRepository.initTransfers(context)
+        BankRepository.initAccounts(context)
         //event is raised [hey guys whoever was observing should get the new data]
         _transfers.postValue(BankRepository.transfers as MutableList<Transfer>)
+        accounts = BankRepository.accounts
     }
 
     fun transfers():LiveData<MutableList<Transfer>> = _transfers
