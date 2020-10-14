8cfca4982bc4ff0d3eed5fffcc925658ae4573cf
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-10-14 15:37:48 +0300

    cortoutine example code

--- Lab8-Interacting with Web API using Coroutines and Retrofit/Tutorial/CoroutinesExample/app/src/main/java/cmps312/lab/coroutinesexample/MainActivity.kt
+++ Lab8-Interacting with Web API using Coroutines and Retrofit/Tutorial/CoroutinesExample/app/src/main/java/cmps312/lab/coroutinesexample/MainActivity.kt
@@ -3,66 +3,72 @@ package cmps312.lab.coroutinesexample
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var job : Job
     val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         startBtn.setOnClickListener {
-            GlobalScope.launch {
-                launch { jump()}
-                launch {run() }
-                launch { playDead() }
-                Log.d(TAG, "Coroutine Thread Name ${Thread.currentThread().name}")
+            job = CoroutineScope(Dispatchers.IO).launch {
+                val j = async { jump() }.await()
+                val r = async { run() }.await()
+                val p = async { playDead() }.await()
+
+                Log.d(TAG, "Jumping is completed: $j")
+                Log.d(TAG, "Runnning is completed: $r")
+                Log.d(TAG, "Playing Dead is completed: $p")
             }
         }
         Log.d(TAG, "mainthread Thread Name ${Thread.currentThread().name}")
+
+        stopBtn.setOnClickListener {
+            job.cancel()
+        }
     }
 
     override fun onPause() {
-        finish();
+        finish();§
         super.onPause()
     }
 
-    suspend fun jump() = withContext(Dispatchers.IO){
-        repeat(1000){
-            val imageId = resources.getIdentifier("jump${it%12+1}" , "drawable" , packageName)
+    suspend fun jump() = withContext(Dispatchers.IO) {
+        repeat(1000) {
+            val imageId = resources.getIdentifier("jump${it % 12 + 1}", "drawable", packageName)
             jumpImg.setImageResource(imageId)
 
-            delay(10)
-            Log.d(TAG, "jump: ")
+//            delay(10)
+//            Log.d(TAG, "jump: ")
         }
     }
 
-    suspend fun run()=withContext(Dispatchers.IO){
-        repeat(1000){
-            val imageId = resources.getIdentifier("run${it%8+1}" , "drawable" , packageName)
+    suspend fun run() = withContext(Dispatchers.IO) {
+        repeat(1000) {
+            val imageId = resources.getIdentifier("run${it % 8 + 1}", "drawable", packageName)
             withContext(Dispatchers.Main) {
                 runImg.setImageResource(imageId)
             }
-            delay(10)
-            Log.d(TAG, "run: ")
+//            delay(10)
+//            Log.d(TAG, "run: ")
         }
     }
 
-    suspend fun playDead()=withContext(Dispatchers.IO){
-        repeat(1000){
-            val imageId = resources.getIdentifier("dead${it%8+1}" , "drawable" , packageName)
+    suspend fun playDead() = withContext(Dispatchers.IO) {
+        repeat(1000) {
+            val imageId = resources.getIdentifier("dead${it % 8 + 1}", "drawable", packageName)
             withContext(Dispatchers.Main) {
                 deadImg.setImageResource(imageId)
             }
-            delay(50)
-            Log.d(TAG, "playDead: ")
+//            delay(50)
+//            Log.d(TAG, "playDead: ")
         }
     }
 }
-
 
 
 /*
