f46b658f98a1b46305ecbdb2357bd9b8cac1f865
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-10-28 22:37:02 +0300

    updated

--- Lab10-Data Management/Tutorial/TodoApplication/app/src/main/java/cmps312/lab/todoapplication/data/local/TodoDao.kt
+++ Lab10-Data Management/Tutorial/TodoApplication/app/src/main/java/cmps312/lab/todoapplication/data/local/TodoDao.kt
@@ -3,12 +3,19 @@ package cmps312.lab.todoapplication.data.local
 import androidx.lifecycle.LiveData
 import androidx.room.*
 import cmps312.lab.todoapplication.data.local.entity.Project
+import cmps312.lab.todoapplication.data.local.entity.ProjectWithTodos
 import cmps312.lab.todoapplication.data.local.entity.Todo
 
 @Dao
 interface TodoDao {
     @Query("SELECT * FROM Project")
+    fun getProjectWithTodos() : List<ProjectWithTodos>
+
+    @Query("SELECT * FROM Project")
     fun getProjects() : LiveData<List<Project>>
+
+    @Query("SELECT * FROM Todo WHERE pid = :pid")
+    fun getTodoListByProjectId(pid : Int) : LiveData<List<Todo>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addProject(project: Project) : Long
@@ -19,8 +26,7 @@ interface TodoDao {
     @Delete
     suspend fun deleteProject(project: Project)
 
-    @Query("SELECT * FROM Todo WHERE pid = :pid")
-    fun getTodoListByProjectId(pid : Int) : LiveData<List<Todo>>
+
 
     @Query("SELECT * FROM Todo WHERE id=:id")
     fun getTodo(id : Int) : Todo
